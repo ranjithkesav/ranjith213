cb0823f93a44b75996a4befd88baf42fd610609a
ranjithkesav <akasharul1725@gmail.com>
2023-08-06 21:40:52 +0530

    libfile commit

---
+++ bb.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>user</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<nav>
+	<img src="lib.jpeg" width="100px" height="100px">
+	<div class="pull-right">
+  <ul class="nav navbar-nav">
+    <li><a href="#">HOME</a></li>
+    <li><a href="#">ABOUT</a></li>
+    <li><a href="#">SERVICE</a></li>
+    <li><a href="#">CONTACT</a></li>
+  </ul>
+  </div>
+</nav>
+</head>
+</html>
